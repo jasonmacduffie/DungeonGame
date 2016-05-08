77c8157dc07d3e852a017e1f85795b4eee2756ac
Jason K. MacDuffie <taknamay@gmail.com>
2016-05-07 23:12:18 -0400

    Allow exiting from a conversation

--- scripts/conversation.gd
+++ scripts/conversation.gd
@@ -13,8 +13,15 @@ func _input(event):
 	var valid = event.is_pressed() && !event.is_echo()
 	if valid:
 		if event.is_action("ui_accept"):
-			# Do something
-			pass
+			enter_option()
+
+func enter_option():
+	var i = get_node("choices").get_selected()
+	if (i + 1) == get_node("choices").get_button_count():
+		get_tree().set_pause(false)
+		queue_free()
+		
+	pass
 
 func set_topic(d):
 	# Set initial to the text box
@@ -27,4 +34,5 @@ func _ready():
 	get_node("name").set_text(npc_name)
 	get_node("avatar").set_texture(load(avatar_location))
 	set_topic(conversation_tree)
+	get_node("choices").grab_focus()
 	set_process_input(true)
