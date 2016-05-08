f11ce00102972bf969ec4179d516e6a0215f7a82
Jason K. MacDuffie <taknamay@gmail.com>
2016-05-08 01:07:53 -0400

    Start room transition logic

--- scripts/transition.gd
+++ scripts/transition.gd
@@ -5,6 +5,15 @@ export var next = ""
 var x
 var y
 
+func _input(event):
+	var valid = event.is_pressed() && !event.is_echo()
+	if valid:
+		if event.is_action("ui_transition"):
+			var player = get_parent().player
+			if player.x == x and player.y == y:
+				print("You changed rooms")
+
 func _ready():
 	pass
+	set_process_input(true)
 
